55b7144edfd45c8c6707f681e25c6096ec8a20e2
renaldin <renaldinoviandi0@gmail.com>
2023-08-28 10:43:50 +0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # test_hitopia_GITS_Indonesia
+
+# Ukuran kompleksitas kodingan pada jawaban No.3:
+
+# Jawaban :
+
+# Ukuran kodingannya menurut saya lebih sederhana karena menggunakan konsep recursive dengan satu function tanpa ada perulangan.
--- balanced_bracket.js
+++ balanced_bracket.js
@@ -1,25 +1,25 @@
-function balancedbracket(string) {
-  const stack = [];
+function balancedbracket(string, stack = []) {
   const bukaBrackets = ["(", "[", "{"];
   const tutupBrackets = [")", "]", "}"];
   const pasangBracket = { ")": "(", "]": "[", "}": "{" };
 
-  for (let i = 0; i < string.length; i++) {
-    const char = string[i];
-    if (bukaBrackets.includes(char)) {
-      stack.push(char);
-    } else if (tutupBrackets.includes(char)) {
-      if (
-        stack.length === 0 ||
-        stack[stack.length - 1] !== pasangBracket[char]
-      ) {
-        return "NO";
-      }
-      stack.pop();
-    }
+  if (string.length === 0) {
+    const result = stack.length === 0 ? "YES" : "NO";
+    return result;
   }
 
-  const result = stack.length === 0 ? "YES" : "NO";
+  const char = string[0];
+
+  if (bukaBrackets.includes(char)) {
+    stack.push(char);
+  } else if (tutupBrackets.includes(char)) {
+    if (stack.length === 0 || stack[stack.length - 1] !== pasangBracket[char]) {
+      return "NO";
+    }
+    stack.pop();
+  }
+
+  const result = balancedbracket(string.slice(1), stack);
 
   return result;
 }
